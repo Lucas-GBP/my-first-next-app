ddaa2f5259dd5cb2ae3e5ad2141316f9bf7d22d7
Lucas-GBP <108595161+Lucas-GBP@users.noreply.github.com>
2022-09-27 11:48:39 -0300

    Learning TailWindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,38 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 
-import { Hello } from '../components/Hello'
-
-interface HomeProps {
-    
-}
-
-export const getStaticProps:GetStaticProps = async (context) =>{
-    return{
-        props:{
-            
-        },
-    }
-}
-
-const Home: NextPage<HomeProps> = (props) => {
-    return (<>
+const Home: NextPage = () => {
+    return <>
         <Head>
             <title>Home</title>
         </Head>
-
-        <header className="bg-black h-16 text-left mb-4">
-            <h1 className="text-5xl pl-5">Home</h1>
+        <header className="bg-black shadow-lg pb-3">
+            <h1 className="text-6xl text-center">Home</h1>
         </header>
         <main className='pl-5'>
-            <Hello />
-
-            <ul>
-                <li className='hover:bg-blue-900'><Link href='/projects/calculatorReact'>Calculator React</Link></li>
-                <li className='hover:bg-blue-900'><Link href='/projects/serverSideRendering'>Server Side Rendering</Link></li>
-            </ul>
+            <div className='fixed left-0 flex flex-col shadow-lg p-2 pt-0 bg-black rounded-b-sm'>
+                <h2 className='text-3xl p-2'>Projects</h2>
+                <Link href='/projects/calculatorReact'>
+                    <a className='w-full p-2 mt-2 text-blue-600 text-2xl
+                        hover:bg-blue-900 hover:text-white rounded-sm hover:underline 
+                        transition-all duration-250'>
+                        Calculator React
+                    </a>
+                </Link>
+                <Link href='/projects/serverSideRendering'>
+                    <a className='w-full p-2 mt-2 text-blue-600 text-2xl
+                        hover:bg-blue-900 hover:text-white rounded-sm hover:underline 
+                        transition-all duration-250'>
+                        Server Side Rendering 
+                    </a>
+                </Link>
+            </div>
         </main>
-        <footer>
-            
-        </footer>
-    </>);
+        <footer className=''>
+            A footer
+        </footer> 
+    </>;
 }
 
 export default Home;
